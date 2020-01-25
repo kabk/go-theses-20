d66b6b4c223903ded0113fd9271040aa3fe746a2
michelangelo <nedislav.kamburov@gmail.com>
2020-01-25 06:32:58 +0100

    whole new website

--- js/script.js
+++ js/script.js
@@ -1,78 +1,40 @@
 document.addEventListener('DOMContentLoaded', ready);
 function ready() {
-  automaticScroll();
-  flipImages();
-  openOtherPages();
+  openContent();
+  openInfo();
+  // flipImages();
+  // openOtherPages();
 }
 
-let stopScrolling;
-let scrollAmount = 12;
-
-function automaticScroll() {
-  let endPoint = document.body.offsetHeight;
-  let currentPoint = window.innerHeight + window.pageYOffset;
-
-  window.addEventListener('resize', () => {
-    endPoint = document.body.offsetHeight;
-  });
-
-  window.addEventListener('wheel', () => {
-    stopScrolling = true;
-    setTimeout(() => {
-      stopScrolling = false;
-    }, 3000);
-  });
-
-  window.addEventListener('keydown', () => {
-    stopScrolling = true;
-    setTimeout(() => {
-      stopScrolling = false;
-    }, 3000);
-  });
-
-  setInterval(function() {
-    if (stopScrolling) {
-      return;
-    } else {
-      currentPoint = window.innerHeight + window.pageYOffset;
-      if (currentPoint - window.innerHeight <= 0 || currentPoint >= endPoint) {
-        scrollAmount = -scrollAmount;
-      }
-      console.log(currentPoint, endPoint);
-      window.scrollBy({
-        top: scrollAmount,
-        behavior: 'smooth'
-      });
-    }
-  }, 20);
-}
-
-function flipImages() {
-  let objects = document.querySelectorAll('.object');
-  objects.forEach(object => {
-    object.addEventListener('click', e => {
-      object.classList.toggle('flipped');
-      stopScrolling = !stopScrolling;
-    });
-  });
-}
-
-function openOtherPages() {
-  let links = document.querySelectorAll('nav.menu > ul > li > a');
-  let objects = document.getElementById('objects');
+function openContent() {
+  const links = document.querySelectorAll('.person');
+  const allContent = document.querySelectorAll('.person-thesis-content');
 
   links.forEach(link => {
     link.addEventListener('click', e => {
-      let link_id = link.id;
-      let link_container = document.querySelector(`div > #${link_id}`);
+      let content = e.target.querySelector('.person-thesis-content');
+      console.log(e);
 
-      if (link_container.style.display == 'none') {
-        link_container.style.display = 'block';
-        objects.style.display = 'none';
+      if (content.style.display == 'block') {
+        allContent.forEach(content => {
+          content.style.display = 'none';
+        });
       } else {
-        link_container.style.display = 'none';
-        objects.style.display = '';
+        allContent.forEach(content => {
+          content.style.display = 'none';
+        });
+        content.style.display = 'block';
       }
     });
+  });
+}
+
+function openInfo() {
+  const infoBtn = document.getElementById('theses-info');
+
+  infoBtn.addEventListener('click', e => {
+    let infoContent = document.getElementById('theses-info-content');
+    infoContent.classList.toggle('visible');
+    infoBtn.classList.toggle('selected');
   });
 }
